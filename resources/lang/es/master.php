b1d0ebfac46f1cbdc74fa2f6cdb5c804161c9c75
pipreman <sudeshnadas.2014@gmail.com>
2025-04-30 11:44:59 +0530

    Add files via upload

---
+++ resources/lang/es/master.php
@@ -0,0 +1,322 @@
+<?php
+
+return
+[
+	'home' => 
+[
+	'banner_heading' => "El mejor lugar para comprar y vender productos digitales",
+	'banner_title' => "Plantillas y gráficos HTML <br/> profesionales y únicos<br /> Marketplace",
+	'choose_category' => "Elegir categoría",
+	'search_template_here' => "Buscar plantilla aquí...",
+	'search' => "Buscar",
+	'Our_Product' => "Nuestros productos",
+	'Check_Out_heading' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscente elit,adipiscing elit,sed do eiusmod.",
+	'Why_Choose_Us' => "¿Por qué elegirnos?",
+	'Why_Choose_heading' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscing.",
+	'featured_product_title' => "Descargar recursos de diseño de kits de interfaz de usuario gratuitos",
+	'featured_product_heading' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscente elit,adipiscing elit,sed do eiusmod.",
+	'testimonials_title' => "Lo que los clientes dicen sobre nosotros",
+	'testimonials_heading' => "Recibe las últimas noticias en tu bandeja de entrada.Consectetur adipiscente elit,adipiscing elit,sed do eiusmod.",
+],	'footer' => 
+[
+	'Mail_Us' => "Envíanos un correo",
+	'Contact_Number' => "Número de contacto",
+	'Our_Location' => "Nuestra Ubicación",
+	'Useful_Links' => "Enlaces útiles",
+	'Home' => "Inicio",
+	'About_Us' => "Acerca de nosotros",
+	'Terms_of_services' => "Términos de servicios",
+	'Privacy_Policy' => "Política de Privacidad",
+	'Contact_Us' => "Contáctanos",
+	'Our_Newsletter' => "Nuestro Boletín",
+	'Enter_your_email' => "Ingrese su correo electrónico aquí",
+],
+
+'header' => 
+[
+'Home' => "Hogar",
+'About_Us' => "Sobre Nosotros",
+'Terms_of_services' => "Términos de Servicio",
+'Privacy_Policy' => "Política de Privacidad",
+'Contact_Us' => "Contáctenos",
+'Products' => "Productos",
+'All_Template' => "Todos los Plantillas",
+'Dashboard' => "Panel de Control",
+'Profile' => "Perfil",
+'My_Order' => "Mis Pedidos",
+'My_Downloads' => "Mis Descargas",
+'Sign_out' => "Cerrar sesión",
+'Login' => "Iniciar sesión",
+'become_an_author' => "Conviértete en Autor",
+	
+],	'product_search' => 
+[
+'title' => "Mira Nuestro Artículo Más Nuevo",
+'heading' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscing elit, sed do eiusmod.",
+'html_templates_sorted' => "Productos",
+'View' => "Ver",
+'Sort_By' => "Ordenar Por",
+'Category' => "Categoría",
+'Price' => "Precio",
+'High' => "Alto",
+'Low' => "Bajo",
+'On_Sale' => "En Oferta",
+'Rating' => "Calificación",
+'Filter' => "Filtrar",
+'Loadmore' => "Cargar Más",
+'add_to_wishlist' => "Agregar a la Lista de Deseos",
+'product_not_found' => "Producto No Encontrado.",
+'author_response' => "Respuesta del Autor",
+'feedback' => "Gracias por tus Comentarios",
+'review' => "No se Encontraron Reseñas.",
+'by' => "Por",
+'search_heading' => "Lorem ipsum dolor sit amet, consecteturead are ipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+'from' => "Desde",
+'live_preview' => "Vista Previa",
+'buy_now' => "Comprar Ahora",
+
+],	'contact_us' => 
+[
+'heading' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscing elit, sed do eiusmod.",
+'contact_information' => "Información de Contacto",
+'number' => "Número de Contacto",
+'here_to_help' => "Estamos Aquí Para Ayudarte",
+'message' => "Mensaje",
+'submit' => "Enviar",
+'name' => "Nombre",
+'email' => "Correo Electrónico",
+'email_address' => "Dirección de Correo Electrónico",
+
+],	'comment_search' => 
+[
+'send_a_reply' => "Enviar una Respuesta",
+'send_reply' => "Enviar Respuesta",
+
+],	'user_profile' => 
+[
+'welcome_back' => "Mi Cuenta y Configuraciones",
+'my_profile' => "Mi Perfil",
+'profile_heading' => "Administra la configuración de tu cuenta, consulta tu historial de pedidos, accede a tus descargas, actualiza tu contraseña y explora oportunidades para convertirte en autor en nuestra plataforma. Personaliza tu experiencia y mantén todo bajo control en un solo lugar.",
+'my_order' => "Mis Pedidos",
+'my_download' => "Mis Descargas",
+'personal_details' => "Datos Personales",
+'contact_email' => "Tu Correo de Contacto",
+'full_name' => "Nombre Completo",
+'user_name' => "Nombre de Usuario",
+'mobile_number' => "Número de Teléfono",
+'country' => "País",
+'select_country' => "Seleccionar País",
+'state' => "Estado",
+'city' => "Ciudad",
+'address' => "Dirección",
+'update' => "Actualizar",
+'change_password' => "Cambiar Contraseña",
+'old_password' => "Contraseña Antigua",
+'confirm_password' => "Confirmar Contraseña",
+'new_password' => "Nueva Contraseña",
+'become_an_author' => "Conviértete en Autor",
+'download' => "Descargar",
+'Review' => "Reseña",
+'no_download_found' => "No se Encontraron Descargas.",
+
+],	'billing_details' => 
+[
+'billing_show' => "Detalles de Facturación",
+'payment_managed' => "Tu Pago es gestionado por Paypal",
+'tnx_number' => "Número de Transacción",
+'amount' => "Monto",
+'payment_date' => "Fecha de Pago",
+'status' => "Estado",
+'not_found' => "No se Encontraron Pedidos.",
+'download' => "Descargar",
+'review' => "Reseña",
+'invoice' => "Factura",
+'orders' => "Pedidos",
+
+],	'review_model' => 
+[
+'product' => "Escribe una reseña para este producto",
+'star_rate' => "Toca una estrella para calificar",
+'5_stars' => "5 estrellas",
+'4_stars' => "4 estrellas",
+'3_stars' => "3 estrellas",
+'2_stars' => "2 estrellas",
+'1_stars' => "1 estrella",
+'post_review' => "Publicar Reseña",
+
+],	'cart' => 
+[
+'your_cart' => "Tu Carrito",
+'add_to_cart' => "Agregar al Carrito",
+'checkout' => "Pagar",
+'done' => "Hecho",
+'add_to_Wishlist' => "Agregar a la Lista de Deseos",
+'continue_to_checkout' => "Continuar con el Pago",
+'no_item_cart' => "No hay artículos en el carrito",
+'add_now' => "Agregar Ahora",
+'recently_viewed' => "Visto Recientemente",
+'remove' => "Eliminar",
+
+],	'wishlist_product' => 
+[
+'wishlist' => "Lista de Deseos",
+'wishlist_list' => "Lista de Deseos",
+'free' => "GRATIS",
+'remove' => "Eliminar",
+'no_item_found' => "No Hay Artículos en la Lista de Deseos",
+'heading_wishlist' => "Recibe las últimas noticias en tu bandeja de entrada. Consectetur adipiscing elit, sed do eiusmod.",
+'recently_viewed' => "Visto Recientemente",
+
+	
+],	'checkout' => 
+[
+'secure_checkout' => "Pago Seguro",
+'payment_details' => "Detalles del Pago",
+'pay_nd_continue' => "Pagar y Continuar",
+'continue' => "Continuar",
+'order_details' => "Detalles del Pedido",
+'total_products' => "Total de Productos",
+'sub_total' => "Subtotal",
+'discount' => "Descuento",
+'total' => "Total",
+'discount_code' => "Aplicar Código de Descuento",
+'apply' => "Aplicar",
+'add_to_cart' => "Agregar al Carrito",
+'product_checkout' => "Pagar",
+'done' => "Hecho",
+'no_payment_required' => "No se requiere pago para este producto.",
+'tax' => "Impuesto",
+'Personal Details' => "Datos Personales",
+
+	
+],	'payment' => 
+[
+'payment_status' => "Estado del Pago",
+'transaction_unsuccessful' => "Transacción No Exitosa",
+'please_try_again' => "Por Favor, Intenta Nuevamente",
+'try_again' => "Intenta Nuevamente",
+'back_to_home' => "Regresar a Inicio",
+'transaction_successful' => "Transacción Exitosa",
+'order_id' => "ID del Pedido",
+'order_status' => "Estado del Pedido",
+'payment_method' => "Método de Pago",
+'date' => "Fecha",
+'add_to_cart' => "Agregar al Carrito",
+'checkout' => "Pagar",
+'done' => "Hecho",
+'products' => "Productos",
+'download' => "Descargar",
+'transaction_in_process' => "Transacción en Proceso",
+'transaction_process_warning' => "Podrás descargar el producto una vez que tengamos la confirmación del pago. Puede tardar hasta 48 horas, dependiendo de la transacción bancaria.",
+
+],	'stripe' => 
+[
+'stripe_payment' => "Pago con Stripe",
+'checkout_forms' => "Formularios de Pago",
+
+	
+],	'login' => 
+[
+'login_account' => "Inicia Sesión en Tu Cuenta.",
+'theme_portal_login' => "Bienvenido de Nuevo. Por Favor, Ingresa Tus Datos de Inicio de Sesión.",
+'email' => "Correo Electrónico",
+'password' => "Contraseña",
+'remember_me' => "Recuérdame",
+'forgot_password' => "¿Olvidaste tu Contraseña?",
+'login' => "Iniciar Sesión",
+'not_account' => "¿No tienes una cuenta?",
+'create_account' => "Crear Cuenta",
+
+],	'register' => 
+[
+'welcome_back_to_theme_portal' => "Bienvenido de Nuevo a AI Sales Converter Store",
+'please_register_to_continue_your_account' => "Por Favor, Regístrate Para Continuar con Tu Cuenta.",
+'name' => "Nombre",
+'email_address' => "Dirección de Correo Electrónico",
+'password' => "Contraseña",
+'confirm_password' => "Confirmar Contraseña",
+'already_have_an_account' => "¿Ya tienes una cuenta?",
+'login' => "Iniciar Sesión",
+'sign_up' => "Regístrate",
+
+],	'reset_password' => 
+[
+'welcome_back_to' => "Bienvenido de Nuevo a",
+'theme_portal' => "AI Sales Converter Store",
+'reset_link' => "Ingresa tu correo electrónico y te enviaremos un enlace para restablecer tu contraseña.",
+'new_pasword' => "Nueva Contraseña",
+'confirm_password' => "Confirmar Contraseña",
+'update' => "Actualizar",
+'login' => "Iniciar Sesión",
+'already_account' => "¿Ya tienes una cuenta?",
+
+],	'forgot_password' => 
+[
+'welcome_back_to' => "Bienvenido de Nuevo a",
+'theme_portal' => "AI Sales Converter Store",
+'reset_link' => "Ingresa tu correo electrónico y te enviaremos un enlace para restablecer tu contraseña.",
+'email_address' => "Dirección de Correo Electrónico",
+'send_password' => "Enviar enlace para restablecer",
+'already_account' => "¿Ya tienes una cuenta?",
+'login' => "Iniciar Sesión",
+
+],	'single_product' => 
+[
+'description' => "Descripción",
+'previews' => "Vistas Previas",
+'reviews' => "Reseñas",
+'comments' => "Comentarios",
+'previews_nd_screenshots' => "Vistas Previas y Capturas de Pantalla",
+'filter_by_rating' => "Filtrar por Calificación",
+'add_comment' => "Agregar un Comentario",
+'post_comment' => "Publicar Comentario",
+'comments_found' => "Comentarios encontrados",
+'sales' => "Ventas",
+'product_details' => "Detalles del Producto",
+'author' => "Autor",
+'india' => "India",
+'theme_portal' => "AI Sales Converter Store",
+'add_to_wishlist' => "Agregar a la Lista de Deseos",
+'add_to_cart' => "Agregar al Carrito",
+'no_comment_found' => "No se Encontró Ningún Comentario.",
+
+],	'gateways' => 
+[
+	'Razorpay_Payment' => "Pago con Razorpay",
+],	'Become_an_author' => 
+[
+'by_continuing_you_agree_to_the' => "Al continuar, aceptas los",
+'author_terms' => "términos del autor de Marktify Market",
+'send_request' => "Enviar Solicitud",
+'Dear' => "Estimado/a",
+'submitted_request_title' => "Gracias por enviar tu solicitud.",
+'Our_administrative_team' => "Nuestro equipo administrativo está revisando tu solicitud y anticipamos que el proceso de aprobación se completará pronto.",
+'Once_your_request_is_approved' => "Una vez que tu solicitud sea aprobada, recibirás una notificación con los detalles necesarios y cualquier información adicional. Agradecemos tu paciencia durante este proceso.",
+'if_your_have_any_que' => "Si tienes alguna pregunta o necesitas más asistencia, no dudes en contactar a nuestro equipo de soporte en",
+'congratulations_title' => "¡Felicidades! Ahora eres un autor.",
+'manage_approved' => "Ahora puedes acceder a tu panel de control para ver y gestionar los detalles aprobados.",
+'my_dashbord' => "Mi Panel de Control",
+'to_access_your_dashboard' => "Para acceder a tu panel de control, inicia sesión en tu cuenta con tus mismas credenciales.",
+
+],	'storage' => 
+[
+'Storage' => "Almacenamiento",
+'s3' => "s3",
+'Wasabi' => "Wasabi",
+'Storage_type' => "Tipo de Almacenamiento",
+'Aws_Access_Key' => "Clave de Acceso de Aws",
+'Aws_Secret_Key' => "Clave Secreta de Aws",
+'Aws_Region' => "Región de Aws",
+'Aws_URL' => "URL de Aws",
+'Aws_Bucket_Name' => "Nombre del Bucket de Aws",
+'Aws_Endpoint' => "Endpoint de Aws",
+'Wasabi_Access_Key' => "Clave de Acceso de Wasabi",
+'Wasabi_Secret_Key' => "Clave Secreta de Wasabi",
+'Wasabi_Region' => "Región de Wasabi",
+'Wasabi_Bucket_Name' => "Nombre del Bucket de Wasabi",
+'Wasabi_Endpoint' => "Endpoint de Wasabi",
+'update' => "Actualizar",
+
+],
+
+];
